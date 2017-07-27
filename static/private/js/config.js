938090201462a4815f27c14b83da5d002febe60b
MrRaph_ <raphael.charrat@gmail.com>
2017-07-27 13:15:37 +0200

    Added test private pages

---
+++ static/private/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_sQdNq6w2Z', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6v7kn5qu9jsh1lno4gh9444b2s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://496d3omdoc.execute-api.eu-west-1.amazonaws.com/prod' // 'https://skpshx15sf.execute-api.eu-west-1.amazonaws.com/prod'
+    }
+}
